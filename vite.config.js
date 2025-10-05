a0b4124f368d7c22f92155bf12c99521f659a422
rubfiber <abhirkarnik@gmail.com>
2025-10-05 12:04:09 -0400

    got rid of ./rubfiber.github.io ruining everything

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-    base: "/react/",
+  plugins: [react({babel: {plugins: [['babel-plugin-react-compiler']],},}),],
+  server: {
+    open: true,
+    port: 3001,
+  },
+  base: "/rubfiber.github.io/",
 });
